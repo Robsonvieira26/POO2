9d1ad53184818e2f02d6af3f8c9ed2489c9fcac1
Robson Vieira <robsonroberto99@gmail.com>
2022-08-04 11:34:16 -0300

    Padrão Factory Method - removendo comentarios

--- src/Entrega_2.java
+++ src/Entrega_2.java
@@ -7,23 +7,24 @@ import personagens.Personagem;
 
 public class Entrega_2 {
   public static void main(String[] args) {
-    // System.out.println("Personagens criados pelo SimplePersonagemFactory");
-    // for (int i = 0; i < 10; i++) {
-    // Personagem personagem = SimplePersonagemFactory.createPersonagem();
-    // System.out.println(personagem.getNome());
-    // System.out.println("Dano:" + personagem.informaDanoCausado() + "\n");
+    System.out.println("Personagens criados pelo SimplePersonagemFactory");
+    for (int i = 0; i < 10; i++) {
+      Personagem personagem = SimplePersonagemFactory.createPersonagem();
+      System.out.println(personagem.getNome());
+      System.out.println("Dano:" + personagem.informaDanoCausado() + "\n");
 
-    // }
-    // System.out.println("Personagens criados pelo AdvancedePersonagemFactory");
-    // for (int i = 0; i < 10; i++) {
-    // Personagem personagem = AdvancedePersonagemFactory.createPersonagem();
-    // System.out.println(personagem.getNome());
-    // System.out.println("Dano:" + personagem.informaDanoCausado() + "\n");
+    }
+    System.out.println("Personagens criados pelo AdvancedePersonagemFactory");
+    for (int i = 0; i < 10; i++) {
+      Personagem personagem = AdvancedePersonagemFactory.createPersonagem();
+      System.out.println(personagem.getNome());
+      System.out.println("Dano:" + personagem.informaDanoCausado() + "\n");
 
-    // }
+    }
     System.out.println("\n------------\nJogo simples:");
     Game gameSimples = new TradicionalGame();
     gameSimples.Jogar();
+
     System.out.println("\n------------\nJogo Avancado:");
     Game gameAvancado = new AdvancedGame();
     gameAvancado.Jogar();
--- src/decorator/ataques_especiais/EstrelaCaida.java
+++ src/decorator/ataques_especiais/EstrelaCaida.java
@@ -6,7 +6,7 @@ import personagens.DanoCausado;
 public class EstrelaCaida extends DanoCausadoDecorador {
   public EstrelaCaida(DanoCausado danoCausado) {
     super(danoCausado);
-    System.out.println("Fogo Aumentando poder em 400%");
+    System.out.println("Estrela Caida Aumentando poder em 400%");
     setDano(4);
   }
 }
